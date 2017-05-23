077e4e28ca88ccdb98bbe201313f63b3256d0237
Ismael de Freitas Pinho <ismaeldefreitas@gmail.com>
2017-05-23 09:50:54 -0300

    Fim do exercício 1.3

--- ex01.3.js
+++ ex01.3.js
@@ -4,8 +4,12 @@ const express = require('express')
 //instanciando o express
 const server = express()
 
-
-
+//método 'route' do express permite encadear várias urls numa mesma instanciação do express
+server.route('/clientes')
+    .get((req, res) => res.send('Lista de Clientes'))
+    .post((req, res) => res.send('Novo Cliente'))
+    .put((req, res) => res.send('Altera Cliente'))
+    .delete((req, res) => res.send('Remove Cliente'))
 
 //alocar uma porta para o servidor express
 server.listen(3000, () => console.log("Executando..."))
